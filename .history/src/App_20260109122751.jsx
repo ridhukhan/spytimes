ac8714989a5fddd5726fd0535ad37efaf6547219
ridhukhan <prakritirshandhane@gmail.com>
2026-01-09 12:33:02 +0600

    update code

---
+++ .history/src/App_20260109122751.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>hello world</h1>
+     <p>qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq<br/>
+     
+     vvfffffffffffffffffffffffffffffffffffffff <br />
+     gggggggggggggggggggggggggggggggggggggggggg <br />
+     
+     </p>
+    </>
+  )
+}
+
+export default App
