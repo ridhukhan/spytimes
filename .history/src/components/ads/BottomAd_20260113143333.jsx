82ad2c553a1efe1eb9155406f5e074934ed16b29
ridhukhan <prakritirshandhane@gmail.com>
2026-01-13 14:34:05 +0600

    push

---
+++ .history/src/components/ads/BottomAd_20260113143333.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef } from "react";
+
+export const BottomAd = () => {
+  const containerRef = useRef(null);
+  const loadedRef = useRef(false);
+
+  useEffect(() => {
+    if (loadedRef.current) return;
+    loadedRef.current = true;
+
+    const script = document.createElement('script');
+    script.src = 'https://pl28446687.effectivegatecpm.com/db/73/91/db7391edcda54695481a78d6831c495e.js';
+    script.async = true;
+    
+    if (containerRef.current) {
+      containerRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="w-full flex justify-center my-4">
+      <div ref={containerRef} className="text-center min-h-[100px]"></div>
+    </div>
+  );
+};
