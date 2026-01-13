ff210f1592ca6d125c56ce0cf1f2a28768e48150
ridhukhan <prakritirshandhane@gmail.com>
2026-01-13 14:21:07 +0600

    update

--- .history/src/components/ads/BottomAd_20260113142017.jsx
+++ .history/src/components/ads/BottomAd_20260113142017.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect} from "react";
 
-export const BottomAd = () => {
+export const BottomSocialBarAd = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://pl28446687.effectivegatecpm.com/db/73/91/db7391edcda54695481a78d6831c495e.js';
