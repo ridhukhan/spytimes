d284d052cfdc9e120cd3742077698e5935c6a020
ridhukhan <prakritirshandhane@gmail.com>
2026-01-14 11:59:05 +0600

    update

---
+++ .history/src/components/ads/FullPageAdBanner_20260114115619.jsx
@@ -0,0 +1,3 @@
+<script async type="application/javascript" src="https://a.pemsrv.com/ad-provider.js"></script> 
+ <ins class="eas6a97888e33" data-zoneid="5827872"></ins> 
+ <script>(AdProvider = window.AdProvider || []).push({"serve": {}});</script>
