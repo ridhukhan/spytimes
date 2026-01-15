f38959cf9ef753b2d23c90e3c0a550f2989a9cb2
ridhukhan <prakritirshandhane@gmail.com>
2026-01-15 13:38:44 +0600

    update

---
+++ .history/src/App_20260115133724.jsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./nav/Navbar"
+import Home from "./pages/home/Home"
+import PrivecyPolicy from "./components/policy/PrivecyPolicy"
+import Terms from "./components/policy/Terms"
+import TrendingDetails from "./components/trending/TrendingDetails"
+import Dieabeties from "./pages/heath/Dieabeties"
+import Health from "./pages/heath/Health"
+import Allergy from "./pages/heath/Allergy"
+import WeightLose from "./pages/heath/WeightLose"
+import Bangla from "./pages/heath/Bangla"
+import Panisgrowth from "./pages/heath/Panisgrowth"
+
+function App() {
+
+  return (
+    <div>
+    
+   <Navbar/>
+ 
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route  path="/privecypolicy" element={<PrivecyPolicy/>}/>
+    <Route  path="/Terms" element={<Terms/>}/> 
+    <Route  path="/Trendingdetails" element={<TrendingDetails/>}/> 
+    <Route path="/blood-sugar-natural-tips" element={<Dieabeties/>}/>
+<Route path="/health" element={<Health/>}/>
+<Route path="/allergy" element={<Allergy/>}/>
+<Route path="/weightlose" element={<WeightLose/>}/>
+<Route path="/secret-solution" element={<Bangla/>}/>
+<Route path="/panis-growth" element={<Panisgrowth/>}/>
+
+
+
+
+   </Routes>
+    </div>
+  )
+}
+
+export default App
